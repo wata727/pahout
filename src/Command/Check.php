d373c2e452d18b20dc5c886341147df0661dacc8
wata_mac <watassbass@gmail.com>
2017-08-11 16:15:22 +0900

    Improve CLI

--- src/Command/Check.php
+++ src/Command/Check.php
@@ -2,6 +2,7 @@
 namespace Pahout\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -10,7 +11,8 @@ class Check extends Command
     protected function configure()
     {
         $this->setName('check')
-             ->setDescription('Check legacy PHP code');
+             ->setDescription('Check legacy PHP code')
+             ->addArgument('files', InputArgument::IS_ARRAY, 'List of file names or directory names to be analyzed');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
--- src/pahout.php
+++ src/pahout.php
@@ -5,6 +5,9 @@ require_once 'vendor/autoload.php';
 use Symfony\Component\Console\Application;
 use Pahout\Command\Check;
 
+$check = new Check();
+
 $app = new Application('Pahout', '0.0.1');
-$app->add(new Check());
+$app->add($check);
+$app->setDefaultCommand($check->getName(), true);
 $app->run();
