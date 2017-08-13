c3f16dd6a0df2c46fac127572fda4483afdb8ec7
wata_mac <watassbass@gmail.com>
2017-08-13 20:51:36 +0900

    :star2:

--- src/Command/Check.php
+++ src/Command/Check.php
@@ -8,6 +8,7 @@ use Pahout\Logger;
 use Pahout\Config;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -17,7 +18,12 @@ class Check extends Command
     {
         $this->setName('check')
              ->setDescription('A linter for writing better PHP')
-             ->addArgument('files', InputArgument::IS_ARRAY, 'List of file names or directory names to be analyzed');
+             ->addArgument('files', InputArgument::IS_ARRAY, 'List of file names or directory names to be analyzed')
+             ->addOption('php-version', null, InputOption::VALUE_OPTIONAL, 'Target PHP version (default: 7.1.8)', null)
+             ->addOption('ignore-tools', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Ignore tool names (default: Nothing to ignore)', null)
+             ->addOption('ignore-paths', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Ignore paths (default: vendor)', null)
+             ->addOption('vendor', null, InputOption::VALUE_NONE, 'Check vendor directory (default: false)', null)
+             ->addOption('format', 'f', InputOption::VALUE_OPTIONAL, 'Output format (default: pretty, possibles: pretty)', null);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
--- src/Config.php
+++ src/Config.php
@@ -15,31 +15,46 @@ class Config
     private const DEFAULT_FILE_PATH = './.pahout.yaml';
 
     private static $config;
-    public $php_version;
-    public $ignore_tools;
-    public $ignore_paths;
-    public $ignore_vendor;
-    public $format;
+    public $php_version = '7.1.8';
+    public $ignore_tools = [];
+    public $ignore_paths = [];
+    public $vendor = false;
+    public $format = 'pretty';
 
     public static function load(array $arguments, string $file = self::DEFAULT_FILE_PATH)
     {
         self::$config = new Config();
-        self::setOption('php_version', $arguments['php_version'] ?? '7.1.8');
-        self::setOption('ignore_tools', $arguments['ignore_tools'] ?? []);
-        self::setOption('ignore_paths', $arguments['ignore_paths'] ?? []);
-        self::setOption('ignore_vendor', $arguments['ignore_vendor'] ?? true);
-        self::setOption('format', $arguments['format'] ?? 'pretty');
 
         if (is_file($file)) {
             Logger::getInstance()->info('Load: '.$file);
             $config_yaml = Yaml::parse(file_get_contents($file));
             foreach ($config_yaml as $key => $value) {
+                // `format` can not be specified from the configuration file.
+                if ($key === 'format') {
+                    throw new InvalidConfigOptionException('format is an invalid option in config file.');
+                }
                 self::setOption($key, $value);
             }
         } elseif ($file !== self::DEFAULT_FILE_PATH) {
             throw new InvalidConfigFilePathException($file.' is not found.');
         } else {
             Logger::getInstance()->info(self::DEFAULT_FILE_PATH.' is not found.');
+        }
+
+        if ($arguments['php-version']) {
+            self::setOption('php_version', $arguments['php-version']);
+        }
+        if ($arguments['ignore-tools']) {
+            self::setOption('ignore_tools', $arguments['ignore-tools']);
+        }
+        if ($arguments['ignore-paths']) {
+            self::setOption('ignore_paths', $arguments['ignore-paths']);
+        }
+        if ($arguments['vendor']) {
+            self::setOption('vendor', $arguments['vendor']);
+        }
+        if ($arguments['format']) {
+            self::setOption('format', $arguments['format']);
         }
     }
 
@@ -78,13 +93,13 @@ class Config
                 }
                 self::$config->ignore_paths = $value;
                 break;
-            case 'ignore_vendor':
+            case 'vendor':
                 if (!is_bool($value)) {
                     throw new InvalidConfigOptionValueException(
-                        $value.' is an invalid ignore_vendor flag. It must be `true` or `false`.'
+                        $value.' is an invalid vendor flag. It must be `true` or `false`.'
                     );
                 }
-                self::$config->ignore_vendor = $value;
+                self::$config->vendor = $value;
                 break;
             case 'format':
                 if (!in_array($value, Formatter::VALID_FORMATS, true)) {
