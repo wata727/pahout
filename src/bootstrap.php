979d839a0dab8db98fb005112d815ec1c7b2b0b2
wata_mac <watassbass@gmail.com>
2017-08-20 19:29:07 +0900

    Add php-ast check in bootstrap script

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,6 +5,17 @@ require_once 'vendor/autoload.php';
 use Symfony\Component\Console\Application;
 use Pahout\Command\Check;
 
+if (extension_loaded('ast')) {
+    $ast_version = phpversion('ast');
+    if (version_compare($ast_version, '0.1.4', '<')) {
+        fprintf(STDERR, "php-ast extension was found. But, Pahout requires php-ast version 0.1.4 or newer. The installed version is $ast_version.\n");
+        exit(Check::EXIT_CODE_ERROR);
+    }
+} else {
+    fprintf(STDERR, "php-ast extension could not be found. Pahout requires php-ast version 0.1.4 or newer.\n");
+    exit(Check::EXIT_CODE_ERROR);
+}
+
 $check = new Check();
 
 $app = new Application('Pahout', '0.0.1');
